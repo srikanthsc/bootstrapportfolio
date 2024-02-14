f28b9ca8f76f71e73c31583e3d19a2936fd22c3c
unknown <srikanthcollaty92@gmail.com>
2024-02-14 10:55:10 +0100

    new updates

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,4 +1,8 @@
-
+/*!
+* Start Bootstrap - Srikanth's Portfolio v6.0.6 (https://startbootstrap.com/theme/stylish-portfolio)
+* Copyright 2013-2024 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-stylish-portfolio/blob/master/LICENSE)
+*/
 window.addEventListener('DOMContentLoaded', event => {
 
     const sidebarWrapper = document.getElementById('sidebar-wrapper');
